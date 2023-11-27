d0e6a4c13dc26fc525d6203e6fc4f843bb823dc0
SZajbuS <dniedziolka1997@gmail.com>
2023-11-27 06:46:42 +0100

    Logger restyling

--- src/classes/Logger.ts
+++ src/classes/Logger.ts
@@ -14,28 +14,30 @@ export default class Logger {
     }
 
     log(msg: string) {
-        this.WriteFile.write(`${this.timestamp()}: ${msg}\n`)
-        console.log(`${chalk.gray(this.timestamp())} ${msg}`);
+        let str = `${this.timestamp()}|INFO: ${msg}`;
+        this.WriteFile.write(str);
+        str = `${chalk.gray(this.timestamp())} ${chalk.greenBright('INFO')} ${msg}`;
+        console.log(str);
     }
 
     success(msg: string) {
-        let str = `${this.timestamp()} SUCCESS: ${msg}`;
+        let str = `${this.timestamp()}|SUCCESS: ${msg}`;
         this.WriteFile.write(str);
-        str = `${chalk.gray(this.timestamp())} ${chalk.bgGreen('SUCCESS')} ${msg}`;
+        str = `${chalk.gray(this.timestamp())} ${chalk.green('SUCCESS')} ${msg}`;
         console.log(str);
     }
 
     failed(msg: string) {
-        let str = `${this.timestamp()} FAILED: ${msg}`;
+        let str = `${this.timestamp()}|FAILED: ${msg}`;
         this.WriteFile.write(str);
-        str = `${chalk.gray(this.timestamp())} ${chalk.bgYellow('FAILED')} ${msg}`;
+        str = `${chalk.gray(this.timestamp())} ${chalk.yellow('FAILED')} ${msg}`;
         console.log(str);
     }
 
     error(msg: string) {
-        let str = `${this.timestamp()} ERROR: ${msg}`;
+        let str = `${this.timestamp()}|ERROR: ${msg}`;
         this.WriteFile.write(str);
-        str = `${chalk.gray(this.timestamp())} ${chalk.bgRed('ERROR')} ${msg}`;
+        str = `${chalk.gray(this.timestamp())} ${chalk.red('ERROR')} ${msg}`;
         console.log(str);
     }
 }
