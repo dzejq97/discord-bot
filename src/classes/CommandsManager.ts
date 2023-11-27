52883fdef4d5081ca0e31b0dc2a8466fcda9a29e
SZajbuS <dniedziolka1997@gmail.com>
2023-11-27 06:43:47 +0100

    seeking for commands function

--- src/classes/CommandsManager.ts
+++ src/classes/CommandsManager.ts
@@ -12,14 +12,14 @@ export default class CommandsManager {
     client: MainClient;
     prefixes: string[];
 
-    // Collection of categories contains collection of commands
-    commands: Collection<string, Collection<string, ICommand>> = new Collection();
+    // Collection of categories contains collection of commands_categories
+    commands_categories: Collection<string, Collection<string, ICommand>> = new Collection();
     constructor(client: MainClient) {
         this.client = client;
         this.prefixes = Prefixes;
 
-        // Loading commands files
-        const categoriesPath = path.join(__dirname, '/../commands');
+        // Loading commands_categories files
+        const categoriesPath = path.join(__dirname, '/../commands_categories');
         const categoriesFolders = fs.readdirSync(categoriesPath);
         for (const category of categoriesFolders) {
             const commandsPath = path.join(categoriesPath, category);
@@ -32,48 +32,44 @@ export default class CommandsManager {
                 const filePath = path.join(commandsPath, file);
                 const command: ICommand = require(filePath).command;
                 command.meta.category = categoryName;
-                this.commands.set(command.meta.category, new Collection<string, ICommand>().set(command.meta.name, command))
+                this.commands_categories.set(command.meta.category, new Collection<string, ICommand>().set(command.meta.name, command))
             }
         }
-        console.log(this.commands);
+        console.log(this.commands_categories);
     }
 
     // Seek for trigger of command in message content
     seekCommand(msg: Message) {
-        
-        // Ignore if command trigger message is DM or message author is bot
         if (!msg.guild || msg.author.bot) return;
 
-        // Search all provided aliases
         let usedPrefix: string = "";
         for (const prefix of this.prefixes) {
             if (msg.content.startsWith(prefix)) usedPrefix = prefix;
         }
-        // Stop if no alias detected
         if (usedPrefix === "") return;
 
-        // Create command trigger context and assign used alias to it
         const context: IContext = {};
         context.used_prefix = usedPrefix;
+        context.message = msg;
+        context.client = this.client;
 
-        // Split command trigger message into arguments
         const commandArgs: string[] = msg.content.substring(usedPrefix.length).split(' ');
         const commandName = commandArgs.shift()?.toLowerCase();
 
-        // Return if cant split message into arguments (maybe only prefix without command?)
-        // TODO: Test it
         if(!commandName) return;
 
-
-
-        this.commands.forEach(category => {
+        this.commands_categories.forEach(category => {
             category.forEach(command => {
                 if (command.meta.name === commandName) {
-                    // TODO: Execute command and insert context
+                    context.used_alias = commandName;
+                    command.execute(context);
+                    return;
                 } else {
                     command.meta.aliases.forEach(alias => {
                         if (alias === commandName) {
-                            // TODO: Execute command and insert context
+                            context.used_alias = commandName;
+                            command.execute(context);
+                            return;
                         }
                     })
                 }
