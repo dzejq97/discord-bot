70ef0ec01a80c5d99f5c41d8b7cb600bd95323bb
SZajbuS <dniedziolka1997@gmail.com>
2023-12-27 07:53:04 +0100

    fixes

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM node:18-alpine
-RUN mkdir -p /usr/bot
-WORKDIR /usr/bot
-COPY .env /usr/bot
-COPY package*.json /usr/bot
-RUN npm i
-COPY . /usr/bot/
-CMD [ "npm", "run", "deploy" ]
--- src/mongo/MongoManager.ts
+++ src/mongo/MongoManager.ts
@@ -8,14 +8,7 @@ import cooldown from "./models/cooldown";
 export default class MongoManager {
     client: CustomClient;
     connection: mongoose.Connection | undefined;
-    auth = {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        username: process.env.DB_ROOT_USERNAME,
-        password: process.env.DB_ROOT_PASSWORD,
-        database_name: process.env.DB_NAME,
-    }
-    connect_URL: string = "";
+    connect_URL: string | undefined = "";
 
     User = user;
     Guild = guild;
@@ -23,20 +16,16 @@ export default class MongoManager {
 
     constructor(client: CustomClient) {
         this.client = client;
-        
-        if (this.client.run_mode === 'dev') {
-            this.auth.host = '127.0.0.1'
-        }
 
-        this.connect_URL = `mongodb://${this.auth.host}:${this.auth.port}/${this.auth.database_name}`
+        this.connect_URL = process.env.DB_URL;
     }
 
     async connect() {
-        this.connection = (await mongoose.connect(this.connect_URL, {
-            authSource: 'admin',
-            user: this.auth.username,
-            pass: this.auth.password,
-        })).connection
+        if (!this.connect_URL) {
+            console.log('Database URL not provided');
+            process.exit(1);
+        }
+        this.connection = (await mongoose.connect(this.connect_URL)).connection
         
     }
 }
