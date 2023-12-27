6f6f1f4d5a332f7bba8bdf02cb390f288b7eb641
SZajbuS <dniedziolka1997@gmail.com>
2023-12-27 14:49:01 +0100

    xxxx

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -8,36 +8,8 @@ export = {
 
     async execute(client: CustomClient) {
         client.logger.success(`Client ready! Logged in as ${client.user?.tag}`);
-        
-        client.logger.info("Synchronizing database");
-        try {
-            (await client.guilds.fetch()).forEach(async OAGuild => {
-                const guild = await OAGuild.fetch();
-
-                if (!await client.mongo.Guild.findOne({ id: guild.id })) {
-                    const g = new client.mongo.Guild({
-                        id: guild.id,
-                        owner_id: guild.ownerId
-                    });
-                    await g.save();
-                }
-    
-                (await guild.members.fetch()).forEach(async (member) => {
-                    if (!await client.mongo.User.findOne({ id: member.user.id })) {
-                        const m = new client.mongo.User({
-                            id: member.user.id,
-                            req_xp: XpStep,
-                        });
-                        await m.save();
-                    }
-                })
-            })
-            //await client.cooldowns.initLoadCooldowns();
-        } catch (error) {
-            client.logger.error(String(error));
-            client.logger.error('Synchronizing failed');
-        }
-        client.logger.success('Synchronized');
+        await client.mongo.synchronize();
+        await client.cooldowns.initLoadCooldowns();
         
     }
 };
--- src/mongo/MongoManager.ts
+++ src/mongo/MongoManager.ts
@@ -15,4 +15,32 @@ export default class MongoManager {
     constructor(client: CustomClient) {
         this.client = client;
     }
+
+    async synchronize() {
+        try {
+            (await this.client.guilds.fetch()).forEach( async (OAGuild) => {
+                const guild = await OAGuild.fetch();
+
+                if (!await this.Guild.find({ id: guild.id })) {
+                    const g = new this.Guild({
+                        id: guild.id,
+                        owner_id: guild.ownerId,
+                    })
+                    await g.save();
+                }
+                
+
+                (await guild.members.fetch()).forEach( async (member) => {
+                    if (!await this.User.find({ id: member.user.id }) && !member.user.bot) {
+                        const m = new this.User({
+                            id: member.user.id,
+                        })
+                        await m.save();
+                    }
+                })
+            })
+        } catch (err) {
+            console.log(String(err));
+        }
+    }
 }
